f8b92a36d08ddb8b71f4755e1c828769689d8409
MichaelKramps <mdk989@gmail.com>
2017-05-11 21:21:28 -0500

    added better solution for 1-1

--- chapter1/1-1.cpp
+++ chapter1/1-1.cpp
@@ -1,6 +1,6 @@
 #include <iostream>
 #include <string>
-#include <unordered_map>
+#include <cstdint>
 
 // Not necessary to assume all ASCII here
 bool allUniqueBruteForce(std::string s)
@@ -16,8 +16,30 @@ bool allUniqueBruteForce(std::string s)
 	return true;
 }
 
+// Now I'll assume the character set is 128 ASCII characters
+// Any string longer than 128 must have duplicates
+
+bool allUnique(std::string s)
+{
+	if (s.length() > 128)
+		return false;
+	bool chars[128] {0};
+	for (int i = 0; i < s.length(); i++)
+	{
+		uint8_t thisChar {static_cast<uint8_t>(s[i])};
+		if (chars[thisChar]) // if we've seen the character already
+		{
+			return false;
+		} else {
+			chars[thisChar] = true;
+		}
+	}
+	return true;
+}
+
 int main()
 {
 	std::cout << allUniqueBruteForce("Unique??") << "\n";
+	std::cout << allUnique("Unique?") << "\n";
 	return 0;
-}	
+}
