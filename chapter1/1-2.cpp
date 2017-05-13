d57b04887b14678994bb02fd6a69f4b31dbc1249
MichaelKramps <mdk989@gmail.com>
2017-05-13 14:49:40 -0500

    committing answer for problem 1-2

--- chapter1/1-2.cpp
+++ chapter1/1-2.cpp
@@ -8,3 +8,44 @@
 // 4. Loop once through each string (2n time) I can build
 //    two arrays (like in the 1-1 solution)
 //    and compare those arrays for equality.
+
+#include <iostream>
+#include <string>
+#include <array>
+#include <cstdint>
+
+void populateCharsArray(std::array<uint16_t, 128> &array, std::string s)
+{
+	for (int i = 0; i < s.length(); i++)
+	{
+		uint8_t index {static_cast<uint8_t>(s[i])};
+		array[index] += 1;
+	}
+}
+
+// Assuming ASCII characters only
+bool isPermutation(std::string a, std::string b)
+{
+	std::array<uint16_t, 128> charsArrayA {0};
+	std::array<uint16_t, 128> charsArrayB {0};
+
+	populateCharsArray(charsArrayA, a);
+	populateCharsArray(charsArrayB, b);
+	
+	if (charsArrayA == charsArrayB)
+	{
+		return true;
+	}
+	else
+	{
+		return false;
+	}
+	
+}
+
+int main()
+{
+	std::cout << isPermutation("permutation of this", "mutation ofis perth") << "\n";
+
+	return 0;
+}
